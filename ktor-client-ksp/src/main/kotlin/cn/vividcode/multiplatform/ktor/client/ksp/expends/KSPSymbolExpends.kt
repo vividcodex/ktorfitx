d9867e9959ae8b719eb560a527311618bbb9c56a
李佳伟 <2965502228@qq.com>
2024-06-22 19:44:02 +0800

    增加 getArgumentClassName 来获取KClass的ClassName

--- ktor-client-ksp/src/main/kotlin/cn/vividcode/multiplatform/ktor/client/ksp/expends/KSPSymbolExpends.kt
+++ ktor-client-ksp/src/main/kotlin/cn/vividcode/multiplatform/ktor/client/ksp/expends/KSPSymbolExpends.kt
@@ -3,6 +3,7 @@ package cn.vividcode.multiplatform.ktor.client.ksp.expends
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSAnnotation
 import com.google.devtools.ksp.symbol.KSDeclaration
+import com.google.devtools.ksp.symbol.KSType
 import com.squareup.kotlinpoet.ClassName
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
@@ -35,14 +36,14 @@ internal fun KSAnnotated.getAnnotationSize(vararg kClasses: KClass<out Annotatio
 	return kClasses.mapNotNull { this.getAnnotation(it) }.size
 }
 
+private val argumentValueCacheMap = mutableMapOf<KSAnnotation, MutableMap<String, Any?>>()
+
 /**
  * 查找注解上的字段
  */
 internal fun <V> KSAnnotation.getArgumentValue(kProperty1: KProperty1<out Annotation, V>): V? {
 	return this.getArgumentValue(kProperty1.name)
 }
-
-private val argumentValueCacheMap = mutableMapOf<KSAnnotation, MutableMap<String, Any?>>()
 
 /**
  * 查找注解上的字段
@@ -52,10 +53,33 @@ internal fun <V> KSAnnotation.getArgumentValue(name: String): V? {
 	val cacheMap = argumentValueCacheMap.getOrPut(this) { mutableMapOf() }
 	return cacheMap.getOrPut(name) {
 		val value = this.arguments.find { it.name?.asString() == name }?.value
+		if (value is KSType) {
+			error("不支持直接获取值")
+		}
 		if (value is ArrayList<*>) {
 			value.toTypedArray()
 		} else value
 	} as? V
+}
+
+/**
+ * 获取注解上的KClass的ClassName
+ */
+internal fun KSAnnotation.getArgumentClassName(kProperty1: KProperty1<out Annotation, KClass<*>>): ClassName? {
+	return this.getArgumentClassName(kProperty1.name)
+}
+
+/**
+ * 获取注解上的KClass的ClassName
+ */
+internal fun KSAnnotation.getArgumentClassName(name: String): ClassName? {
+	val cacheMap = argumentValueCacheMap.getOrPut(this) { mutableMapOf() }
+	return cacheMap.getOrPut(name) {
+		val value = this.arguments.find { it.name?.asString() == name }?.value
+		if (value is KSType) {
+			value.declaration.className
+		} else error("不支持使用此类型")
+	} as? ClassName
 }
 
 /**
