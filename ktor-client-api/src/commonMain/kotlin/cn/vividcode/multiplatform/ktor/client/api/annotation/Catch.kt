d7e3f16ee9db44611cea8f682ccfe2904332c317
李佳伟 <2965502228@qq.com>
2024-07-04 23:59:47 +0800

    新增：@Catch 和 @Finally 计划

---
+++ ktor-client-api/src/commonMain/kotlin/cn/vividcode/multiplatform/ktor/client/api/annotation/Catch.kt
@@ -0,0 +1,14 @@
+package cn.vividcode.multiplatform.ktor.client.api.annotation
+
+/**
+ * 项目：vividcode-multiplatform-ktor-client
+ *
+ * 作者：li-jia-wei
+ *
+ * 创建：2024/7/4 下午11:57
+ *
+ * 介绍：Catch
+ */
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.SOURCE)
+annotation class Catch
---
+++ ktor-client-api/src/commonMain/kotlin/cn/vividcode/multiplatform/ktor/client/api/annotation/Finally.kt
@@ -0,0 +1,14 @@
+package cn.vividcode.multiplatform.ktor.client.api.annotation
+
+/**
+ * 项目：vividcode-multiplatform-ktor-client
+ *
+ * 作者：li-jia-wei
+ *
+ * 创建：2024/7/4 下午11:57
+ *
+ * 介绍：Finally
+ */
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.SOURCE)
+annotation class Finally
