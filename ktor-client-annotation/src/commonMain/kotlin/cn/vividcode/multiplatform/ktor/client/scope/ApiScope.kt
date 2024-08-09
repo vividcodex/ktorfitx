ecfc50a17f2c30d0f7b9b38f150c8b6c4aa48b2c
李佳伟 <2965502228@qq.com>
2024-08-10 00:56:08 +0800

    新功能：ApiScope 支持定义名称以及声明

--- ktor-client-annotation/src/commonMain/kotlin/cn/vividcode/multiplatform/ktor/client/scope/ApiScope.kt
+++ ktor-client-annotation/src/commonMain/kotlin/cn/vividcode/multiplatform/ktor/client/scope/ApiScope.kt
@@ -7,11 +7,29 @@ package cn.vividcode.multiplatform.ktor.client.scope
  *
  * 创建日期：2024/8/4 23:40
  *
- * 文件介绍：ApiScope
+ * 文件介绍：Api 作用域
  */
-interface ApiScope
+interface ApiScope {
+	
+	/**
+	 * Api 作用域名称
+	 */
+	val name: String
+		get() = this::class.simpleName!!
+	
+	/**
+	 * Api 作用域声明，第一次使用打印，可通过配置关闭
+	 */
+	val declaration: String
+		get() = "这是 ${this::class.simpleName!!}"
+}
 
 /**
- * 默认 ApiScope
+ * 默认的 ApiScope
  */
-object DefaultApiScope : ApiScope
+object DefaultApiScope : ApiScope {
+	
+	override val name: String = "默认的接口作用域"
+	
+	override val declaration: String = "默认的接口作用域，不建议使用"
+}
