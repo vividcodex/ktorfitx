461b758017158573835e07994ca656b7421971a0
李佳伟 <2965502228@qq.com>
2025-07-17 09:57:54 +0800

    feat: 添加不同包路径的的测试路由

---
+++ server-sample/src/main/kotlin/cn/ktorfitx/server/sample/routes2/ComplexTest.kt
@@ -0,0 +1,35 @@
+package cn.ktorfitx.server.sample.routes2
+
+import cn.ktorfitx.server.annotation.*
+import cn.ktorfitx.server.annotation.Authentication
+import cn.ktorfitx.server.annotation.Principal
+import io.ktor.http.content.*
+import io.ktor.server.auth.*
+
+@Authentication
+@POST("complex/test1")
+fun complexTest1(
+	@Field field: String,
+	@Principal principal: UserIdPrincipal,
+	@Query query: String,
+): String = ""
+
+@Authentication
+@POST("complex/{name}")
+fun complexTest2(
+	@PartForm part: String,
+	@PartForm("custom1") part2: String,
+	@PartFile file: ByteArray?,
+	@PartBinary binary: PartData.BinaryItem,
+	@PartBinaryChannel binaryChannel: PartData.BinaryChannelItem,
+	@Principal principal: UserIdPrincipal,
+	@Query query: String,
+	@Query("custom2") query2: Int,
+	@Path name: String,
+	@Cookie cookie: String,
+	@Cookie("custom3") cookie2: String?,
+	@Header authentication: String,
+	@Header("Custom4") header1: String?,
+	@Attribute attribute1: Int,
+	@Attribute("custom5") attribute2: String?,
+): String = ""
